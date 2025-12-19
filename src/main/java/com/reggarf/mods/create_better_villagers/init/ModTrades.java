90267559d3de6e6e8f7f651de24e12e9c5c01272
A1phaE <lpshunt199@gmail.com>
2025-12-19 19:01:16 +0200

    Update ModTrades.java

--- src/main/java/com/reggarf/mods/create_better_villagers/init/ModTrades.java
+++ src/main/java/com/reggarf/mods/create_better_villagers/init/ModTrades.java
@@ -31,20 +31,20 @@ public class ModTrades {
                 event.getTrades().get(1).add(new BasicItemListing(new ItemStack(Items.EMERALD, 2), new ItemStack(AllBlocks.GEARBOX, 2), 3, 5, 0.07f));
                 event.getTrades().get(1).add(new BasicItemListing(new ItemStack(Items.EMERALD, 2), new ItemStack(AllBlocks.SHAFT, 4), 3, 5, 0.07f));
                 event.getTrades().get(1).add(new BasicItemListing(new ItemStack(Items.EMERALD, 2), new ItemStack(AllBlocks.LARGE_COGWHEEL, 2), 3, 5, 0.07f));
-                event.getTrades().get(1).add(new BasicItemListing(new ItemStack(Items.EMERALD, 1), new ItemStack(AllItems.ANDESITE_ALLOY, 4), 3, 5, 0.07f));
+                event.getTrades().get(1).add(new BasicItemListing(new ItemStack(Items.EMERALD, 1), new ItemStack(AllItems.ANDESITE_ALLOY.get(), 4), 3, 5, 0.07f));
                 //LEVEL 2
                 event.getTrades().get(2).add(new BasicItemListing(new ItemStack(Items.EMERALD, 3), new ItemStack(AllBlocks.ANDESITE_FUNNEL, 5), 4, 5, 0.05f));
                 event.getTrades().get(2).add(new BasicItemListing(new ItemStack(Items.EMERALD, 3), new ItemStack(AllBlocks.ANDESITE_TUNNEL, 4), 2, 5, 0.05f));
                 event.getTrades().get(2).add(new BasicItemListing(new ItemStack(Items.EMERALD, 1), new ItemStack(AllBlocks.COGWHEEL, 3), 4, 5, 0.05f));
                 //LEVEL 3
-                event.getTrades().get(3).add(new BasicItemListing(new ItemStack(Items.EMERALD, 4), new ItemStack(AllItems.IRON_SHEET, 6), 3, 5, 0.07f));
-                event.getTrades().get(3).add(new BasicItemListing(new ItemStack(Items.EMERALD, 2), new ItemStack(AllItems.ROSE_QUARTZ, 4), 3, 5, 0.05f));
+                event.getTrades().get(3).add(new BasicItemListing(new ItemStack(Items.EMERALD, 4), new ItemStack(AllItems.IRON_SHEET.get(), 6), 3, 5, 0.07f));
+                event.getTrades().get(3).add(new BasicItemListing(new ItemStack(Items.EMERALD, 2), new ItemStack(AllItems.ROSE_QUARTZ.get(), 4), 3, 5, 0.05f));
                 //LEVEL 4
-                event.getTrades().get(4).add(new BasicItemListing(new ItemStack(Items.EMERALD, 4), new ItemStack(AllItems.FILTER, 2), 3, 5, 0.05f));
-                event.getTrades().get(4).add(new BasicItemListing(new ItemStack(Items.EMERALD, 5), new ItemStack(AllItems.ATTRIBUTE_FILTER, 2), 3, 5, 0.05f));
+                event.getTrades().get(4).add(new BasicItemListing(new ItemStack(Items.EMERALD, 4), new ItemStack(AllItems.FILTER.get(), 2), 3, 5, 0.05f));
+                event.getTrades().get(4).add(new BasicItemListing(new ItemStack(Items.EMERALD, 5), new ItemStack(AllItems.ATTRIBUTE_FILTER.get(), 2), 3, 5, 0.05f));
                 //LEVEL 5
-                event.getTrades().get(5).add(new BasicItemListing(new ItemStack(Items.EMERALD, 1), new ItemStack(AllItems.PROPELLER, 2), 3, 5, 0.05f));
-                event.getTrades().get(5).add(new BasicItemListing(new ItemStack(Items.EMERALD, 1), new ItemStack(AllItems.WHISK, 2), 3, 5, 0.05f));
+                event.getTrades().get(5).add(new BasicItemListing(new ItemStack(Items.EMERALD, 1), new ItemStack(AllItems.PROPELLER.get(), 2), 3, 5, 0.05f));
+                event.getTrades().get(5).add(new BasicItemListing(new ItemStack(Items.EMERALD, 1), new ItemStack(AllItems.WHISK.get(), 2), 3, 5, 0.05f));
 
             }
         }
@@ -56,7 +56,7 @@ public class ModTrades {
             if (event.getType() == ModVillagers.BRASSWORKER.value()) {
                 //LEVEL 1
 
-                event.getTrades().get(1).add(new BasicItemListing(new ItemStack(Items.EMERALD, 1), new ItemStack(AllItems.BRASS_INGOT, 2), 5, 4, 0.07f));
+                event.getTrades().get(1).add(new BasicItemListing(new ItemStack(Items.EMERALD, 1), new ItemStack(AllItems.BRASS_INGOT.get(), 2), 5, 4, 0.07f));
                 event.getTrades().get(1).add(new BasicItemListing(new ItemStack(Items.EMERALD, 4), new ItemStack(AllBlocks.SEQUENCED_GEARSHIFT, 2), 3, 4, 0.07f));
                 //LEVEL 2
                 event.getTrades().get(2).add(new BasicItemListing(new ItemStack(Items.EMERALD, 3), new ItemStack(AllBlocks.MECHANICAL_CRAFTER, 2), 4, 5, 0.05f));
@@ -66,10 +66,10 @@ public class ModTrades {
                 event.getTrades().get(3).add(new BasicItemListing(new ItemStack(Items.EMERALD, 2), new ItemStack(AllBlocks.TRACK, 6), 3, 5, 0.05f));
                 //LEVEL 4
                 event.getTrades().get(4).add(new BasicItemListing(new ItemStack(Items.EMERALD, 3), new ItemStack(AllBlocks.RAILWAY_CASING, 4), 3, 5, 0.05f));
-                event.getTrades().get(4).add(new BasicItemListing(new ItemStack(Items.EMERALD, 3), new ItemStack(AllItems.BRASS_INGOT, 5), 3, 5, 0.05f));
+                event.getTrades().get(4).add(new BasicItemListing(new ItemStack(Items.EMERALD, 3), new ItemStack(AllItems.BRASS_INGOT.get(), 5), 3, 5, 0.05f));
                 //LEVEL 5
                 event.getTrades().get(5).add(new BasicItemListing(new ItemStack(Items.EMERALD, 2), new ItemStack(AllBlocks.SMART_FLUID_PIPE, 2), 10, 5, 0.05f));
-                event.getTrades().get(5).add(new BasicItemListing(new ItemStack(Items.EMERALD, 5), new ItemStack(AllItems.BRASS_HAND, 2), 4, 5, 0.05f));
+                event.getTrades().get(5).add(new BasicItemListing(new ItemStack(Items.EMERALD, 5), new ItemStack(AllItems.BRASS_HAND.get(), 2), 4, 5, 0.05f));
                 event.getTrades().get(5).add(new BasicItemListing(new ItemStack(Items.EMERALD, 2), new ItemStack(AllBlocks.DISPLAY_LINK, 2), 10, 5, 0.05f));
             }
         }
@@ -80,7 +80,7 @@ public class ModTrades {
             if (event.getType() == ModVillagers.COPPERWORKER.value()) {
                 //LEVEL 1
 
-                event.getTrades().get(1).add(new BasicItemListing(new ItemStack(Items.EMERALD, 2), new ItemStack(AllItems.COPPER_SHEET, 3), 5, 5, 0.07f));
+                event.getTrades().get(1).add(new BasicItemListing(new ItemStack(Items.EMERALD, 2), new ItemStack(AllItems.COPPER_SHEET.get(), 3), 5, 5, 0.07f));
                 event.getTrades().get(1).add(new BasicItemListing(new ItemStack(Items.EMERALD, 1), new ItemStack(AllBlocks.FLUID_PIPE, 2), 3, 5, 0.07f));
 
                 //LEVEL 2
@@ -105,19 +105,18 @@ public class ModTrades {
             if (event.getType() == ModVillagers.MINER.value()) {
                 //LEVEL 1
                 event.getTrades().get(1).add(new BasicItemListing(new ItemStack(Items.EMERALD, 2), new ItemStack(Blocks.ANDESITE, 7), 10, 5, 0.07f));
-                event.getTrades().get(1).add(new BasicItemListing(new ItemStack(Items.EMERALD, 2), new ItemStack(AllBlocks.LIMESTONE, 7), 10, 5, 0.07f));
                 event.getTrades().get(1).add(new BasicItemListing(new ItemStack(Items.EMERALD, 2), new ItemStack(AllBlocks.DEPOT, 2), 10, 5, 0.07f));
                 //LEVEL 2
                 event.getTrades().get(2).add(new BasicItemListing(new ItemStack(Items.EMERALD, 2), new ItemStack(AllItems.ZINC_INGOT, 4), 10, 5, 0.05f));
-                event.getTrades().get(2).add(new BasicItemListing(new ItemStack(Items.EMERALD, 6), new ItemStack(AllBlocks.CRUSHING_WHEEL, 2), 10, 5, 0.05f));
+                event.getTrades().get(2).add(new BasicItemListing(new ItemStack(Items.EMERALD, 6), new ItemStack(AllBlocks.CRUSHING_WHEEL.get(), 2), 10, 5, 0.05f));
                 //LEVEL 3
                 event.getTrades().get(3).add(new BasicItemListing(new ItemStack(Items.EMERALD, 3), new ItemStack(Blocks.TORCH, 20), 10, 5, 0.07f));
                 event.getTrades().get(3).add(new BasicItemListing(new ItemStack(Items.EMERALD, 9), new ItemStack(Blocks.IRON_BLOCK, 2), 10, 5, 0.05f));
                 //LEVEL 4
-                event.getTrades().get(4).add(new BasicItemListing(new ItemStack(Items.EMERALD, 3), new ItemStack(AllItems.SWEET_ROLL, 4), 10, 5, 0.05f));
-                event.getTrades().get(4).add(new BasicItemListing(new ItemStack(Items.EMERALD, 3), new ItemStack(AllItems.BUILDERS_TEA, 2), 10, 5, 0.05f));
+                event.getTrades().get(4).add(new BasicItemListing(new ItemStack(Items.EMERALD, 3), new ItemStack(AllItems.SWEET_ROLL.get(), 4), 10, 5, 0.05f));
+                event.getTrades().get(4).add(new BasicItemListing(new ItemStack(Items.EMERALD, 3), new ItemStack(AllItems.BUILDERS_TEA.get(), 2), 10, 5, 0.05f));
                 //LEVEL 5
-                event.getTrades().get(5).add(new BasicItemListing(new ItemStack(Items.EMERALD, 5), new ItemStack(AllItems.ZINC_INGOT, 8), 4, 5, 0.05f));
+                event.getTrades().get(5).add(new BasicItemListing(new ItemStack(Items.EMERALD, 5), new ItemStack(AllItems.ZINC_INGOT.get(), 8), 4, 5, 0.05f));
                 event.getTrades().get(5).add(new BasicItemListing(new ItemStack(Items.EMERALD, 5), new ItemStack(Items.COPPER_INGOT, 12), 4, 5, 0.05f));
             }
         }
